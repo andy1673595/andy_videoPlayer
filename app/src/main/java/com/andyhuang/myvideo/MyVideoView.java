9cb8afc36d3d48d5ea73a31be35f825fa812cff7
Andy_Huang <andy5953761@gmail.com>
2018-04-27 12:10:00 +0800

    完成直的

---
+++ app/src/main/java/com/andyhuang/myvideo/MyVideoView.java
@@ -0,0 +1,64 @@
+package com.andyhuang.myvideo;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.VideoView;
+
+public class MyVideoView extends VideoView{
+    int video_height = 0;
+    int video_width = 0;
+    int layout_height =0;
+    int layout_width = 0;
+    int heightAfter=0;
+    int widthAfter=0;
+    boolean getVideo = false;
+    public MyVideoView(Context context) {
+        super(context);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //first
+        if(layout_height == 0) {
+            layout_height = getHeight();
+            layout_width = getWidth();
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+
+        if( ( layout_height != getHeight()|| layout_width != getWidth() ) && !getVideo) {
+            video_height = getHeight();
+            video_width = getWidth();
+            getVideo = true;
+            if(video_width >= video_height) {
+                widthAfter = layout_width;
+                heightAfter = (video_height * widthAfter) / video_width;
+                int x = 0;
+                x ++;
+            } else {
+                heightAfter = layout_width;
+                widthAfter = (video_width*heightAfter)/video_height;
+            }
+        }
+
+        if(getVideo) {
+            setMeasuredDimension( widthAfter, heightAfter);
+        }else{
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+      //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+      /*  int w = getWidth();
+        int h = getHeight();
+        int width = getDefaultSize(getWidth(), widthMeasureSpec);
+        int height = getDefaultSize(getHeight(), heightMeasureSpec);*/
+        Log.d("myvideo","v w"+video_width+", v h" +video_height+",l w "+layout_width+", la h "+layout_height);
+       // setMeasuredDimension(width, height);
+    }
+
+}
--- app/src/main/java/com/andyhuang/myvideo/VideoPlayer.java
+++ app/src/main/java/com/andyhuang/myvideo/VideoPlayer.java
@@ -1,37 +1,67 @@
 package com.andyhuang.myvideo;
 
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.net.Uri;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
 
+import java.io.IOException;
+
+
 public class VideoPlayer extends AppCompatActivity {
-    private VideoView vidView;
-    private MediaController vidControl;
-    String vidAddress = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
-
-
+    private MyVideoView vidView;
+    private android.widget.MediaController vidControl;
+    String vidAddress = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/protraitVideo.mp4";
+   // String vidAddress = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
+    MediaSessionCompat mMediaSession;
+    PlaybackStateCompat.Builder mStateBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-
-
         setContentView(R.layout.video_player);
-        vidView = (VideoView) findViewById(R.id.myVideo);
+        vidView = (MyVideoView) findViewById(R.id.myVideo);
+
+
         MediaSession session = new MediaSession(this,"myvideo");
-        vidControl = new MediaController(this,session.getSessionToken());
-        MediaController.TransportControls transportControls = vidControl.getTransportControls();
-        MediaControllerCompat
+
+
+      //  vidControl = new android.widget.MediaController(this);
+      //  MediaController.TransportControls transportControls = vidControl.getTransportControls();
+
+     /*   mMediaSession = new MediaSessionCompat(this, "myplayer");
+
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        mMediaSession.setMediaButtonReceiver(null);
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mMediaSession.setPlaybackState(mStateBuilder.build());*/
+
+
         Uri vidUri = Uri.parse(vidAddress);
         vidView.setVideoURI(vidUri);
-        vidView.start();
 
-   //     transportControls.play();
+        int height = vidView.getHeight();
+        int width = vidView.getWidth();
+
+        vidView.start();
+         height = vidView.getHeight();
+         width = vidView.getWidth();
+
+        int x = 0;
+        x++;
     }
 }
